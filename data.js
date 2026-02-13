f8afdc52931380ee6e25e2279953dda9be2d7285
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2026-02-13 14:14:55 -0800

    Add attendee: Alina Singh

--- data.js
+++ data.js
@@ -21,6 +21,11 @@ var attendeeData = [
         "link": "https://www.linkedin.com/"
     },
     {
+        "name": "Alina Singh",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
         "name": "Aisha Doyle",
         "image": "",
         "link": "https://www.linkedin.com/"
