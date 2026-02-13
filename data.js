9ce6682fedb4cdccdbac5d43f80488feafc1485e
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2026-02-13 14:13:33 -0800

    Add attendee: Tobias Ward

---
+++ data.js
@@ -0,0 +1,17 @@
+// <STARTDATA>\nvar attendeeData = [
+    {
+        "name": "Add Your Name",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
+        "name": "Tobias Ward",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
+        "name": "Another Attendee",
+        "image": "",
+        "link": "https://github.com/"
+    }
+];\n// <ENDDATA>
