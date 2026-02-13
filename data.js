e88bf83df4fc532b174c088731de26abb00bf189
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2026-02-13 14:14:57 -0800

    Add attendee: Blake Lopez

--- data.js
+++ data.js
@@ -26,6 +26,11 @@ var attendeeData = [
         "link": "https://www.linkedin.com/"
     },
     {
+        "name": "Blake Lopez",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
         "name": "Kai Walker",
         "image": "",
         "link": "https://www.linkedin.com/"
