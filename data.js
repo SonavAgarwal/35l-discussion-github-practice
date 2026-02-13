ec5a1c991cbad365214bdbe128a4abec88543296
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2026-02-13 14:16:22 -0800

    Update attendees

--- data.js
+++ data.js
@@ -336,6 +336,11 @@ var attendeeData = [
         "link": "https://www.linkedin.com/"
     },
     {
+        "name": "Zion Quinn",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
         "name": "Rafael Rogers",
         "image": "",
         "link": "https://www.linkedin.com/"
