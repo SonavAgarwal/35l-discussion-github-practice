8c11379cd1146d21cf00f57cab1fe7301b5537dc
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2026-02-13 14:16:21 -0800

    Update attendees

--- data.js
+++ data.js
@@ -181,6 +181,11 @@ var attendeeData = [
         "link": "https://www.linkedin.com/"
     },
     {
+        "name": "Remy Han",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
         "name": "Amir Mendoza",
         "image": "",
         "link": "https://www.linkedin.com/"
