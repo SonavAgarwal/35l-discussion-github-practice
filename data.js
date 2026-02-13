da3db1e5bfefa01dc180900ac046b0a639c947f7
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2026-02-13 14:14:55 -0800

    Add attendee: Paige Watson

--- data.js
+++ data.js
@@ -46,6 +46,11 @@ var attendeeData = [
         "link": "https://github.com/"
     },
     {
+        "name": "Paige Watson",
+        "image": "",
+        "link": "https://www.linkedin.com/"
+    },
+    {
         "name": "Sawyer Rogers",
         "image": "",
         "link": "https://www.linkedin.com/"
